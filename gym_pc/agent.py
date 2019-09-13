116b8bd25b24bc745feefeb988e2e7fb5cfe2f10
Nicolas Gastellu <nico@ordinateur-de-nico-5.local>
2019-09-13 12:42:25 -0400

    Updated agent

--- gym_pc/agent.py
+++ gym_pc/agent.py
@@ -1,13 +1,31 @@
 import pytesseract
 import random
+import numpy as np
 
 from gym_pc.envs import LinuxEnv
 
+
 if __name__ == '__main__':
+
+    with open('commands.txt') as fo:
+        COMMAND_LIST = fo.readlines()
+    
+    epsilon = 0.9
+    q_list = np.ones(len(COMMAND_LIST))
+    n_list = np.ones(len(COMMAND_LIST))
+    
+
     env = LinuxEnv()
     while True:
-        action = random.choice(["test", "asdf"])
-        img, reward, _, _ = env.step(action)
+        #action = random.choice(["test", "asdf"])
+        p = np.random.random()
+        if p > epsilon:
+            action_index = np.argmax(q_list)
+        else:
+            action_index = np.random.choice(np.arange(len(COMMAND_LIST)),p=q_list/sum(n_list))
+        img, reward, _, _ = env.step(COMMAND_LIST[action_index])
+        n_list[action_index] += 1
+        q_list[action_index] = q_list[action_index] + (reward - q_list[action_index])/n_list[action_index]
         # env.render()
         wrd = pytesseract.image_to_string(img, lang="eng", config="--psm 4 --oem 3 -c tessedit_char_whitelist=command")
         print('Action: ' + action + ', Reward: ' + str(reward))
